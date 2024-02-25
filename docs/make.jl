590e73ad48e0e4d5e5798423ba9dbb275b110b26
Tianyi Pu <912396513@qq.com>
2024-02-25 12:56:25 +0000

    add documenter workflow

--- docs/make.jl
+++ docs/make.jl
@@ -10,6 +10,6 @@ using SwapSort
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "https://github.com/putianyi889/SwapSort.jl.git"
+)
